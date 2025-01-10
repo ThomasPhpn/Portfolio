af78e9f082134af59a10cdecc9853a707ab6195b
Thomas Philipponneau <tphilipponneau@gmail.com>
2025-01-10 21:26:41 +0100

    contenu texte header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-moonstone text-white py-6">
+    <footer className="bg-moonstone text-white py-6 px-4">
       <div className="container mx-auto text-center">
         <p>&copy; 2025 Thomas Philipponneau. Tous droits réservés.</p>
       </div>
